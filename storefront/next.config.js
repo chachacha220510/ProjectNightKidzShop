d29cd964264b038e522cfdb626ed9a80c0dab153
chachacha220510 <chachacha220510@gmail.com>
2025-05-16 13:51:15 -0400

    Update next.config.js for initial deployment

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -1,10 +1,11 @@
 const checkEnvVariables = require("./check-env-variables")
-
 checkEnvVariables()
 
-/**
- * @type {import('next').NextConfig}
- */
+// 预处理 hostnames，避免 undefined 报错
+const minioHost = process.env.NEXT_PUBLIC_MINIO_ENDPOINT?.replace(/^https?:\/\//, '')
+const medusaHost = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL?.replace(/^https?:\/\//, '')
+const baseUrlHost = process.env.NEXT_PUBLIC_BASE_URL?.replace(/^https?:\/\//, '')
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
@@ -18,37 +19,36 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        
       },
-      { // Note: needed to serve images from /public folder
+      ...(baseUrlHost ? [{
         protocol: process.env.NEXT_PUBLIC_BASE_URL?.startsWith('https') ? 'https' : 'http',
-        hostname: process.env.NEXT_PUBLIC_BASE_URL?.replace(/^https?:\/\//, ''),
-      },
-      { // Note: only needed when using local-file for product media
+        hostname: baseUrlHost,
+      }] : []),
+      ...(medusaHost ? [{
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL?.replace('https://', ''),
-      },
-      { // Note: can be removed after deleting demo products
+        hostname: medusaHost,
+      }] : []),
+      ...(minioHost ? [{
+        protocol: "https",
+        hostname: minioHost,
+      }] : []),
+      {
         protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
-      { // Note: can be removed after deleting demo products
+      {
         protocol: "https",
         hostname: "medusa-server-testing.s3.amazonaws.com",
       },
-      { // Note: can be removed after deleting demo products
+      {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
-      ...(process.env.NEXT_PUBLIC_MINIO_ENDPOINT ? [{ // Note: needed when using MinIO bucket storage for media
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_MINIO_ENDPOINT,
-      }] : []),
     ],
   },
   serverRuntimeConfig: {
-    port: process.env.PORT || 3000
-  }
+    port: process.env.PORT || 3000,
+  },
 }
 
 module.exports = nextConfig
